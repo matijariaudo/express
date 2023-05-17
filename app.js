4e0fc50367e2c5d08c2f57dac5a7ab786bfbef24
Mati Ariaudo <98933246+matijariaudo@users.noreply.github.com>
2023-05-16 22:02:44 -0500

    tengo los huevos al plato 4

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const { Server } = require("./models/server");
-const { Client } = require("whatsapp-web.js");
+const { Client , LocalAuth} = require("whatsapp-web.js");
 console.clear()
 
 const app=new Server();
 app.listen();
-
+  
 let QR;
-const client = new Client();
-console.log("Comenzando con ","33333");
+nro="5493406460886";
+const client = new Client({authStrategy: new LocalAuth({ clientId: nro }), puppeteer: {headless: true,args: ['--no-sandbox']}});
+console.log("Comenzando con: ",nro);
 client.on('qr', (qr) => {
-QR=qr; 
-console.log('QR RECEIVED', qr);
+  QR=qr; 
+  console.log('QR RECEIVED', qr);
 });
 client.on('ready', () => {
-console.log('Client is ready!');
+  console.log('Client is ready!');
 });
 client.on('message',msg=>{
-console.log(msg)
+  console.log(msg.from,msg.body)
 })
 client.initialize();
 
